6c8d98a2ebf9bc8cf381a56ee1a5422ab191ade3
Nam Nguyen <97155032+namnguyen429030@users.noreply.github.com>
2025-08-25 15:05:32 +0700

    remove duplicated properties

--- SAPLSServer/DTOs/Concrete/ParkingLotDtos/ParkingLotDetailsDto.cs
+++ SAPLSServer/DTOs/Concrete/ParkingLotDtos/ParkingLotDetailsDto.cs
@@ -5,13 +5,9 @@ namespace SAPLSServer.DTOs.Concrete.ParkingLotDtos
 {
     public class ParkingLotDetailsDto : ParkingLotSummaryDto
     {
-        public string? Description { get; set; }
-        public int TotalParkingSlot { get; set; }
         public ParkingLotOwnerProfileDetailsDto ParkingLotOwner { get; set; }
         public ParkingLotDetailsDto(ParkingLot parkingLot) : base(parkingLot)
         {
-            Description = parkingLot.Description;
-            TotalParkingSlot = parkingLot.TotalParkingSlot;
             ParkingLotOwner = new ParkingLotOwnerProfileDetailsDto(parkingLot.ParkingLotOwner);
         }
     }
