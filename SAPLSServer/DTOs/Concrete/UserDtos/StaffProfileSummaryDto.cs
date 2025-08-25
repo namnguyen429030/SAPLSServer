e36fe50d95d7f1a431213f732ff545739d0e4ddf
Nam Nguyen <97155032+namnguyen429030@users.noreply.github.com>
2025-08-25 23:31:51 +0700

    update staff id for dto

--- SAPLSServer/DTOs/Concrete/UserDtos/StaffProfileSummaryDto.cs
+++ SAPLSServer/DTOs/Concrete/UserDtos/StaffProfileSummaryDto.cs
@@ -4,10 +4,12 @@ namespace SAPLSServer.DTOs.Concrete.UserDtos
 {
     public class StaffProfileSummaryDto : UserSummaryDto
     {
+        public string StaffId { get; set; }
         public string ParkingLotId { get; set; }
 
         public StaffProfileSummaryDto(StaffProfile staff) : base(staff.User)
         {
+            StaffId = staff.StaffId;
             ParkingLotId = staff.ParkingLotId;
         }
     }
