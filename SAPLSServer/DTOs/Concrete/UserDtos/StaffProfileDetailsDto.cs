f168290b02c2d12d0c05aa90e8ac8a4222c8b14c
Nam Nguyen <97155032+namnguyen429030@users.noreply.github.com>
2025-08-26 11:18:23 +0700

    add staff id

--- SAPLSServer/DTOs/Concrete/UserDtos/StaffProfileDetailsDto.cs
+++ SAPLSServer/DTOs/Concrete/UserDtos/StaffProfileDetailsDto.cs
@@ -5,9 +5,11 @@ namespace SAPLSServer.DTOs.Concrete.UserDtos
     public class StaffProfileDetailsDto : UserDetailsDto
     {
         public string ParkingLotId { get; set; }
+        public string StaffId { get; set; }
 
         public StaffProfileDetailsDto(StaffProfile staff) : base(staff.User)
         {
+            StaffId = staff.StaffId;
             ParkingLotId = staff.ParkingLotId;
         }
     }
